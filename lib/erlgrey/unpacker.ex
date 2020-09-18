c379a60136ae6e13682b5e792ca9dad550ee732b
Corey Powell <mistdragon100@gmail.com>
2020-09-17 22:08:44 -0500

    Added 64 bit tests

--- lib/erlgrey/unpacker.ex
+++ lib/erlgrey/unpacker.ex
@@ -1,14 +1,28 @@
 defmodule Erlgrey.Unpacker.Helpers do
+  @moduledoc """
+  Some helpers for the unpacking module
+  """
+
+  @spec capped_range(integer) :: list | Range.t()
   def capped_range(0) do
+    # seems weird to use an empty list doesn't it?
+    # this is the only enumerable off the top of my head that I can use to effectively give a
+    # true 0 element range
     []
   end
 
   def capped_range(x) do
+    # otherwise construct the range like normal
     1..x
   end
 end
 
 defmodule Erlgrey.Unpacker32 do
+  @moduledoc """
+  Unpacking module for 32bit based packed data.
+
+  This module assumes that all integers are 32 bits in length.
+  """
   import Erlgrey.Unpacker.Helpers
 
   def unpack(<<"B",size::big-signed-integer-size(32),str::binary-size(size),rest::binary>>) do
@@ -77,6 +91,11 @@ defmodule Erlgrey.Unpacker32 do
 end
 
 defmodule Erlgrey.Unpacker64 do
+  @moduledoc """
+  Unpacking module for 32bit based packed data.
+
+  This module assumes that all integers are 64 bits in length.
+  """
   import Erlgrey.Unpacker.Helpers
 
   def unpack(<<"B",size::big-signed-integer-size(64),str::binary-size(size),rest::binary>>) do
--- test/erlgrey_test.exs
+++ test/erlgrey_test.exs
@@ -72,4 +72,76 @@ defmodule ErlgreyTest do
       assert {:ok, :binary, ""} = Erlgrey.unpack32(result)
     end
   end
+
+  describe "(un)pack64/2" do
+    test "can (un)pack a binary" do
+      result = Erlgrey.pack64("Hello, World", return: :binary)
+      assert <<"B",12::signed-big-integer-size(64),"Hello, World">> == result
+      assert {:ok, "Hello, World", ""} == Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack an integer" do
+      for x <- [-2147483648, -1, 0, 1, 2147483647] do
+        result = Erlgrey.pack64(x, return: :binary)
+        assert <<"I",x::signed-big-integer-size(64)>> == result
+        assert {:ok, x, ""} == Erlgrey.unpack64(result)
+      end
+    end
+
+    test "can (un)pack a float" do
+      for x <- [-7643.946, -1.3, 0.0, 1.3, 4678.632] do
+        result = Erlgrey.pack64(x, return: :binary)
+        assert <<"F",x::big-float-size(64)>> == result
+        assert {:ok, y, ""} = Erlgrey.unpack64(result)
+        # fuzzy match
+        assert Float.round(x, 2) == Float.round(y, 2)
+      end
+    end
+
+    test "can (un)pack an empty list" do
+      x = []
+      result = Erlgrey.pack64(x, return: :binary)
+      assert <<"L",0::signed-big-integer-size(64)>> == result
+      assert {:ok, x, ""} == Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack a list" do
+      x = ["Hello",1,12.43,[],%{}]
+      result = Erlgrey.pack64(x, return: :binary)
+      assert <<"L",5::signed-big-integer-size(64),_rest::binary>> = result
+      assert {:ok, ["Hello",1,f,[],%{}], ""} = Erlgrey.unpack64(result)
+      assert Float.round(f, 2) == Float.round(12.43, 2)
+    end
+
+    test "can (un)pack a map" do
+      x = %{1 => "Hello", "world" => "Dah", 2 => ["a"]}
+      result = Erlgrey.pack64(x, return: :binary)
+      assert <<"M",3::signed-big-integer-size(64),_rest::binary>> = result
+      assert {:ok, %{1 => "Hello", "world" => "Dah", 2 => ["a"]}, ""} = Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack a nil value" do
+      result = Erlgrey.pack64(nil, return: :binary)
+      assert <<"0">> = result
+      assert {:ok, nil, ""} = Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack a true value" do
+      result = Erlgrey.pack64(true, return: :binary)
+      assert <<"T">> = result
+      assert {:ok, true, ""} = Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack a false value" do
+      result = Erlgrey.pack64(false, return: :binary)
+      assert <<"F">> = result
+      assert {:ok, false, ""} = Erlgrey.unpack64(result)
+    end
+
+    test "can (un)pack an atom value" do
+      result = Erlgrey.pack64(:binary, return: :binary)
+      assert <<"A",6::signed-big-integer-size(64),_rest::binary>> = result
+      assert {:ok, :binary, ""} = Erlgrey.unpack64(result)
+    end
+  end
 end
